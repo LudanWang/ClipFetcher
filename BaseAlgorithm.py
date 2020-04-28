0bb369193ea5caf9a711eca054dd0c51a25e74f3
asiahowtone <enandy0225@gmail.com>
2020-04-28 23:41:39 +0800

    Update BaseAlgorithm.py
    將回傳改成兩個並修正部分錯誤

--- BaseAlgorithm.py
+++ BaseAlgorithm.py
@@ -8,10 +8,7 @@ def frequencyAlgo(vod_id):#a秒內有b個留言
     f = open(filename, "r", encoding="utf-8")  # filename
     y = json.loads(f.read())
 
-    # 計算每一個時間點的留言頻率
-    freqCount=[0]*int(y['comment'][-1]['time']+1)
-    print(y['comment'][-1]['time'])
-    print(freqCount)
+    freqCount=[0]*int(y['comment'][-1]['time']+1)#計算每一個時間點的時間頻率
     for i in range(len(y['comment'])):
         freqCount[int(y['comment'][i]['time'])]+=1
 
@@ -20,72 +17,33 @@ def frequencyAlgo(vod_id):#a秒內有b個留言
     freq997=mean+3*sd #99.7%
     freq95=mean+2*sd #95%
 
-    maxfreq=[]
-    for i in range(len(freqCount)):
-        if freqCount[i] == max(freqCount):
-            maxfreq.append(i)
-    print(',',maxfreq)
-    print(len(maxfreq))
-
-    maxStart=[]
-    maxEnd=[]
-    i=0
-    while i < len(maxfreq)-2: #數到倒數第三個
-        if maxfreq[i] + 15 > maxfreq[i+1]:
-            maxStart.append(maxfreq[i]-10)
-            for j in range(i+1,len(maxfreq)-1):
-                if maxfreq[j]+15<=maxfreq[j+1]:
-                    break
-            maxEnd.append(maxfreq[j] + 5)
-            i = j +1
-        else:
-            maxStart.append(maxfreq[i]-10)
-            maxEnd.append(maxfreq[i]+5)
-            i+=1
-    print('maxS',maxStart)
-    print('maxE',maxEnd)
-    print(len(maxStart))
-
-    durationMax=[]
-    for i in range(len(maxStart)):
-        durationMax.append(maxEnd[i]-maxStart[i]+1)
-    print('d,',durationMax)
-    s=0
-    for i in durationMax:
-        s+=i
-    print(s)
-
-
-    #各頻率符合的秒數
-    #常態分佈大於兩個標準差的時間點
+    #個頻率符合的秒數
     freq95Time=[]
     for i in range(len(freqCount)):
         if freqCount[i]>=freq95:
             freq95Time.append(i)
-    # 常態分佈大於三個標準差的時間點
     freq997Time=[]
     for i in range(len(freqCount)):
         if freqCount[i]>=freq997:
             freq997Time.append(i)
 
+#標準差3 997%
     freq997Start=[]
     freq997End=[]
     i=0
-    while i <= len(freq997Time)-2: #數到倒數第三個
-        if freq997Time[i]  > freq997Time[i+1]:
-            freq997Start.append(freq997Time[i]-7)
+    while i < len(freq997Time)-2: #數到倒數第三個
+        if freq997Time[i] + 15 > freq997Time[i+1]:
+            freq997Start.append(freq997Time[i]-10)
             for j in range(i+1,len(freq997Time)-1):
-                if freq997Time[j]<=freq997Time[j+1]:
+                if freq997Time[j]+15<=freq997Time[j+1]:
                     break
-            freq997End.append(freq997Time[j]  - 7)
+            freq997End.append(freq997Time[j] + 5)
             i = j +1
         else:
-            freq997Start.append(freq997Time[i] - 7)
-            freq997End.append(freq997Time[i]  - 7)
+            freq997Start.append(freq997Time[i]-10)
+            freq997End.append(freq997Time[i]+5)
             i+=1
-    print(freq997Start)
-    print(freq997End)
-
+#標準差2 95%
     freq95Start=[]
     freq95End=[]
     i=0
@@ -101,47 +59,58 @@ def frequencyAlgo(vod_id):#a秒內有b個留言
             freq95Start.append(freq95Time[i]-10)
             freq95End.append(freq95Time[i]+5)
             i+=1
-    print('95start',freq95Start)
-    print(freq95End)
 
-    Start=[]
-    End=[]
-    for i in range(len(freq95Start)-1):#外 有在裡面就記下來
-        for j in range(len(freq997Start)-1):
-            if ( freq997Start[j]>=freq95Start[i] ) and ( freq997End[j]<=freq95End[i] ):
-                if len(Start)==0:
-                    Start.append(freq95Start[i])
-                    End.append(freq95End[i])
-                else:
-                    if freq95Start[i]!=Start[-1]:
-                        Start.append(freq95Start[i])
-                        End.append(freq95End[i])
-    print(Start)
-    print(End)
-    duration=[]
-
-    if len(Start)==0:
-        Start = freq95Start
-        for i in range(len(Start)):
-            duration.append(freq95End[i] - freq95Start[i] + 1)
+#找尋精華片段 開始及時間點
+    tempStart=[]
+    tempEnd=[]
+    if len(freq997Start)!=0:
+        for i in range(len(freq95Start)-1):#外 有在裡面就記下來
+            for j in range(len(freq997Start)-1):#
+                if ( freq997Start[j]>=freq95Start[i] ) and ( freq997End[j]<=freq95End[i] ):
+                    if len(tempStart)==0:
+                        tempStart.append(freq95Start[i])
+                        tempEnd.append(freq95End[i])
+                    else:
+                        if freq95Start[i]!=tempStart[-1]:
+                            tempStart.append(freq95Start[i])
+                            tempEnd.append(freq95End[i])
     else:
-        for i in range(len(Start)):
-            duration.append(End[i]-Start[i]+1)
-    sum=0
-    for i in duration:
-        sum+=i
-    print(sum)
-    print(strftime("%H:%M:%S", gmtime(sum)))
-    print(len(Start))
-    print(End)
-    timestamp=[Start,duration]
-    for i in range(len(timestamp[0])):
-        timestamp[0][i]=strftime("%H:%M:%S", gmtime(timestamp[0][i]))
-        timestamp[1][i]=strftime("%H:%M:%S", gmtime(timestamp[1][i]))
-    print(timestamp[0])
-    print(timestamp[1])
-    Start=strftime("%H:%M:%S", gmtime(timestamp[0][i]))
-    duration=strftime("%H:%M:%S", gmtime(timestamp[1][i]))
-    return Start,duration#回傳list list[0]為開始時間 list[1]為結束時間 以hh:mm:ss字串
+        for i in range(len(freq95Start)):
+            tempStart.append(freq95Start[i])
+            tempEnd.append(freq95End[i])
+    # print('start',tempStart)
+    # print('end',tempEnd)
+    tempduration=[]
+    for i in range(len(tempStart)):
+        tempduration.append(tempEnd[i]-tempStart[i]+1)
+    # print('duration',tempduration)
+
+#得到前幾長的精華片段 要找前幾長還要再研究
+    longestClipIndex = sorted(sorted(range(len(tempduration)), key=lambda sub: tempduration[sub])[-7:])#計算前幾長的
+    ssum=0
+    for i in range(len(longestClipIndex)):
+        ssum+=tempduration[longestClipIndex[i]]
+        # print(tempStart[longestClipIndex[i]],tempduration[longestClipIndex[i]])
+    # print('cliplength',strftime("%H:%M:%S", gmtime(ssum)))#clip長度
+
+#最後輸出 開始時間 及 影片長度
+    start=[]
+    duration=[]
+    for i in range(len(longestClipIndex)):
+        start.append(tempStart[longestClipIndex[i]])
+        duration.append(tempduration[longestClipIndex[i]])
+    # print('start',start)
+    # print('duration',duration)
+    #timestamp=[Start,duration]
+    for i in range(len(start)):
+        start[i]=strftime("%H:%M:%S", gmtime(start[i]))
+        duration[i]=strftime("%H:%M:%S", gmtime(duration[i]))
+    # print(start)
+    # print(duration)
+
+    return start,duration
+
+
+
 
 
