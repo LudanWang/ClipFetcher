46123ec51960b34a2ac8a97135b8c5bc20168a6b
asiahowtone <enandy0225@gmail.com>
2020-05-27 22:13:41 +0800

    錯誤修改

--- ChatFileHandler.py
+++ ChatFileHandler.py
@@ -6,30 +6,32 @@ import os
 VIDEO_FOLDER_ID = '1tcs1nW3eWhXhC150nJhRK_g-KyH1d6Nr'
 CHAT_FOLDER_ID = '1np-YmrUitiIoenl7hWuzgHYVQOTfK3k2'
 
-def UploadChat(vod_id):
+def UploadChat(vod_id,abs_path):
     gauth = GoogleAuth()
     scope = ['https://www.googleapis.com/auth/drive']
     gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('ClipFetcher-SERVICE-ACCOUNT.json', scope)
     drive = GoogleDrive(gauth)
 
-    file_name = vod_id + '.json'
-    with open(file_name, "r") as file:
+    file_name = vod_id + '.json' #
+    with open(abs_path, "r") as file:
         file_drive = drive.CreateFile({'title': os.path.basename(file.name),
                                        'parents': [{'kind': 'drive#fileLink',
                                                     'id': CHAT_FOLDER_ID}]})
+        file_drive.SetContentFile(abs_path)
         file_drive.Upload()
 
-def UploadVideo(vod_id):
+def UploadVideo(vod_id,abs_path):
     gauth = GoogleAuth()
     scope = ['https://www.googleapis.com/auth/drive']
     gauth.credentials = ServiceAccountCredentials.from_json_keyfile_name('ClipFetcher-SERVICE-ACCOUNT.json', scope)
     drive = GoogleDrive(gauth)
 
     file_name = vod_id + '.mp4'
-    with open(file_name, "r") as file:
+    with open(abs_path, "r") as file:
         file_drive = drive.CreateFile({'title': os.path.basename(file.name),
                                         'parents': [{'kind': 'drive#fileLink',
                                                      'id': VIDEO_FOLDER_ID}]})
+        file_drive.SetContentFile(abs_path)
         file_drive.Upload()
 
 def ListFile(folder_id):
